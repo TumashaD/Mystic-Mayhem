70bc7da91a590209c0ac3b7c25b1f85ad21c0416
Savinu Anusara <savinu0002@gmail.com>
2024-03-11 00:05:26 +0530

    same thing as last time

--- Character/Archer/ArchersList.java
+++ Character/Archer/ArchersList.java
@@ -1,10 +1,10 @@
-package Character.Archer;
-import Character.Character.Category;
+// package Character.Archer;
+// import Character.Character.Category;
 
-public class ArchersList {
-    public Archer Shooter = new Archer("Shooter",Category.Highlander, 80, 11, 4, 6, 9);
-    public Archer Ranger = new Archer("Ranger", Category.Highlander, 115, 14, 5, 8, 10);
-    public Archer Sunfire = new Archer("Sunfire", Category.Sunchild, 160, 15, 5, 7, 14);
-    public Archer Zing = new Archer("Zing", Category.Sunchild, 200, 16, 9, 11, 14);
-    public Archer Saggitarius = new Archer("Saggitarius", Category.Mystic, 230, 18, 7, 12, 17);
-}
+// public class ArchersList {
+//     public Archer Shooter = new Archer("Shooter",Category.Highlander, 80, 11, 4, 6, 9);
+//     public Archer Ranger = new Archer("Ranger", Category.Highlander, 115, 14, 5, 8, 10);
+//     public Archer Sunfire = new Archer("Sunfire", Category.Sunchild, 160, 15, 5, 7, 14);
+//     public Archer Zing = new Archer("Zing", Category.Sunchild, 200, 16, 9, 11, 14);
+//     public Archer Saggitarius = new Archer("Saggitarius", Category.Mystic, 230, 18, 7, 12, 17);
+// }
