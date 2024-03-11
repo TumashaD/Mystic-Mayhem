7a6008288e775dc8531af3c8145525fd911dd182
Savinu Anusara <savinu0002@gmail.com>
2024-03-11 09:40:55 +0530

    Remove character classes and update Character class

--- Character/Character.java
+++ Character/Character.java
@@ -1,16 +1,52 @@
 package Character;
 
-abstract public class Character {
-    public enum Category {
+public abstract class Character {
+    protected enum Category {
         Highlander, Marshlander,Sunchild,Mystic
     }
-    public String name;
-    public int price;
-    public int attack;
-    public int defense;
-    public int health;
-    public int speed;
-    public Category category;
+    protected String name;
+    protected int price;
+    protected int attack;
+    protected int defense;
+    protected int health;
+    protected int speed;
+    protected Category category;
+    
+    public void attack(int damage){
+        health -= 0.5*damage - 0.1*this.defense;
+    }
+
+    public void heal(int healing_power){
+        this.health += 0.1*healing_power;
+    }
+
+    public void equipment(int attack, int defense, int health, int speed, int price){
+        this.attack += attack;
+        this.defense += defense;
+        this.health += health;
+        this.speed += speed;
+        this.price += price*0.2;
+    }
+
+    public void setBattleGround(String battleGround){
+        switch (battleGround) {
+            case "Hillcrest":
+                
+                break;
+            case "Marshland":
+                    
+                    break;
+            case "Desert":
+                
+                break;
+            case "Arcane":
+                
+                break;
+        
+            default:
+                break;
+        }
+    }
 }
 
 
--- Character/Mage/Mage.java
+++ Character/Mage/Mage.java
@@ -2,13 +2,43 @@ package Character.Mage;
 import Character.Character;
 
 public class Mage extends Character{
-    public Mage(String name,Category category, int price, int attack, int defense, int health, int speed) {
-        this.name = name;
-        this.category = category;
-        this.price = price;
-        this.attack = attack;
-        this.defense = defense;
-        this.health = health;
-        this.speed = speed;
+    public Mage(String name) {
+      switch (name) {
+        case "Warlock":
+            this.name = name;
+            this.category = Category.Marshlander;
+            this.price = 100;this.attack = 12;this.defense = 7;this.health = 10;this.speed = 12;
+            break;
+
+        case "Sorcerer":
+            this.name = name;
+            this.category = Category.Marshlander;
+            this.price = 130;this.attack = 14;this.defense = 8;this.health = 11;this.speed = 13;
+            break;
+        
+        case "Enchanter":
+            this.name = name;
+            this.category = Category.Highlander;
+            this.price = 160;this.attack = 16;this.defense = 10;this.health = 13;this.speed = 16;
+            break;
+        
+        case "Conjurer":
+            this.name = name;
+            this.category = Category.Highlander;
+            this.price = 195;this.attack = 18;this.defense = 15;this.health = 14;this.speed = 12;
+            break;
+        
+        case "Eldritch":
+            this.name = name;
+            this.category = Category.Mystic;
+            this.price = 270;this.attack = 19;this.defense = 17;this.health = 18;this.speed = 14;
+            break;
+
+        
+      
+        default:
+            System.out.println("Invalid Mage name. Please choose from the list.");
+            break;
+      }
     }
 }
